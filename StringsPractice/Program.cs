157337407e5b203ecfa8c4ae42bd0b0e7df53758
danielvivacqua <drvivacqua@hotmail.com>
2017-06-29 15:06:30 -0400

    Completed several in-class exercises

--- StringsPractice/Program.cs
+++ StringsPractice/Program.cs
@@ -10,6 +10,45 @@ namespace StringsPractice
     {
         static void Main(string[] args)
         {
+            //string firstName = "Daniel";
+            //int lengthOfName = firstName.Length;
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(lengthOfName);
+
+            //string myFaveQuote = "\"It's a yam sham\" - Buffy Summers";
+            //Console.WriteLine(myFaveQuote);
+
+            //string escapeString = "I want a tab after this line \t I want a new line without WriteLine \nHi";
+            //string escapeString2 = "I want to show a backslash here \\ Hey what's that noise? \a";
+            //Console.WriteLine(escapeString);
+            //Console.WriteLine(escapeString2);
+
+            //Console.WriteLine("User one, please type something");
+            //string userOneInput = Console.ReadLine();
+            //Console.WriteLine("User two, please type something");
+            //string userTwoInput = Console.ReadLine();
+
+            //if (userOneInput.Equals(userTwoInput))
+            //{
+            //    Console.WriteLine("User 2 is a copy cat!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Way to be original in your own way!");
+            //}
+
+            // string startOfSentence = "My best friend's name is ";
+            // string endOfSentence = "Bethany.";
+            // string fullSentence = startOfSentence + endOfSentence;
+            //// fullSentence = string.Concat(startOfSentence, endOfSentence);
+
+            // Console.WriteLine(fullSentence);
+
+            // Console.WriteLine("My friend " + endOfSentence + " has a cute kid.");
+            // Console.WriteLine("The first part of my sentence was {0}, the second was {1}, and the third was {2}", startOfSentence, endOfSentence, fullSentence);
+
+            Console.WriteLine("This is a tab. \t \nThis is a backspace \\.\nAnd this is a bell sound \a.");
+
         }
     }
 }
